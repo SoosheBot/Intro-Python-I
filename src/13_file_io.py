4c21da826afbf436dd3dee7fca024417b2bd678e
sooshe <sooshe@sooshebot.com>
2020-05-10 19:30:43 -0500

    added text to wrong file--recopied it to foo.txt

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,7 +9,13 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
+#MY NOTES -- r, w --> read, write. .close() closes the file, 'with open() as __' opens the file
+
 # YOUR CODE HERE
+# Code for printing to a file 
+with open('/Users/Mahadevi/Documents/CS7/python1/Intro-Python-I/src/foo.txt', 'r') as file:
+    print(file.read())
+    file.close()
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -17,3 +23,10 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+with open('/Users/Mahadevi/Documents/CS7/python1/Intro-Python-I/src/foo.txt', 'w') as file:
+    print(file.write('We demand...a SHRUBBERY'))
+    file.close()
+
+with open('/Users/Mahadevi/Documents/CS7/python1/Intro-Python-I/src/foo.txt', 'r') as file:
+    print(file.read())
+    file.close()
